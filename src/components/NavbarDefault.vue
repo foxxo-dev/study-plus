27b1031c58d4e1c5ce02b20f057802969374bcf6
Adam Buksowicz <gammert54@gmail.com>
2025-02-07 09:47:21 +0100

    Added a Landing Page

---
+++ src/components/NavbarDefault.vue
@@ -0,0 +1,124 @@
+<template>
+  <nav>
+    <h3>Study+</h3>
+    <div class="nav_links">
+      <router-link to="/dashboard">Home</router-link>
+      <div class="dot"></div>
+      <router-link to="/dashboard">About</router-link>
+      <div class="dot"></div>
+      <router-link to="/dashboard">Contact</router-link>
+      <div class="dot"></div>
+      <router-link to="/dashboard">Learn More</router-link>
+    </div>
+    <div>
+      <button>Join Premium</button>
+      <button popovertarget="profile_selector" class="popover_toggle">
+        <img
+          :src="
+            photoUrl == null
+              ? 'https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg'
+              : photoUrl
+          "
+          alt="profile"
+          id="profile_anchor" />
+      </button>
+    </div>
+  </nav>
+  <div id="spacer___"></div>
+</template>
+
+<script>
+export default {
+  props: {
+    photoUrl: {
+      type: String,
+      default: null,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.nav_links {
+  font-size: 1.25rem;
+  gap: 1rem;
+}
+.popover_toggle {
+  all: unset !important;
+  anchor-name: --profile_anchor !important;
+}
+h3 {
+  font-family: 'League Spartan', serif;
+  font-size: 1.3rem;
+}
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  background-image: linear-gradient(to bottom, #00000081, #0000);
+  color: white;
+  width: 100%;
+  position: fixed;
+  height: 6rem;
+  z-index: 9999;
+}
+nav div {
+  display: flex;
+  gap: 2rem;
+  height: 2.75rem;
+  justify-content: center;
+  align-items: center;
+}
+
+img {
+  height: 3rem;
+  border-radius: 50%;
+  aspect-ratio: 1;
+  cursor: pointer;
+}
+
+button {
+  all: unset;
+  background: #e2af08;
+  padding-inline: 2rem;
+  font-family: 'Inter', sans-serif;
+  font-weight: 500;
+  border-radius: 10vw;
+  height: 2.75rem;
+  font-size: 1.3rem;
+  cursor: pointer;
+}
+
+a {
+  color: white;
+  text-decoration: none;
+  position: relative;
+}
+
+a::after {
+  content: '';
+  display: block;
+  width: 0;
+  height: 2px;
+  background: white;
+  transition: width 0.3s;
+  position: absolute;
+  bottom: -0.25rem;
+}
+a:hover::after {
+  width: 100%;
+}
+
+.dot {
+  height: 0.5rem;
+  width: 0.5rem;
+  background: white;
+  border-radius: 50%;
+}
+
+#spacer___ {
+  height: 6rem;
+  width: 100%;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,132 @@
 <template>
+  <div id="background_color"></div>
+  <NavbarDefault />
   <main>
-    <router-link to="/login">Login</router-link>
+    <img src="@/assets/img/moutains.png" alt="mountains" class="bg" />
+    <div class="landing">
+      <h1 class="right">
+        TAKE YOUR <br />
+        LEARNING TO THE <br />
+        PEAKS
+      </h1>
+      <div class="landing_bottom">
+        <p>
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters, as opposed to using 'Content here.
+        </p>
+        <button @click="navigateToLogin">Get Started</button>
+      </div>
+    </div>
+    <h1 class="seperator">LEARN MORE, EASIER</h1>
+    <img src="@/assets/img/sand.png" alt="sand" class="sand_bg" />
   </main>
 </template>
 
 <script>
+import NavbarDefault from '@/components/NavbarDefault.vue';
+
 export default {
-  methods: {},
+  methods: {
+    navigateToLogin() {
+      this.$router.push('/login');
+    },
+  },
+  components: {
+    NavbarDefault,
+  },
 };
 </script>
 
 <style scoped>
+.seperator {
+  height: 10rem;
+  background: #4e43bb;
+  width: 101vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transform: translateY(-4rem) rotate(4.12deg) translateX(-0.5rem);
+}
+#background_color {
+  width: 100%;
+  height: 100%;
+  background: #3f1487;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -999999;
+  right: 0;
+  bottom: 0;
+}
+.landing_bottom {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: white;
+}
+.landing_bottom button {
+  height: 90%;
+  width: 30rem;
+}
+button {
+  all: unset;
+  background: #ffffff33;
+  backdrop-filter: blur(1rem);
+  border-radius: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.5rem;
+  text-decoration: none;
+  font-family: 'League Spartan', serif;
+  -webkit-box-shadow: 7px 7px 20px 0px rgba(0, 0, 0, 0.25);
+  -moz-box-shadow: 7px 7px 20px 0px rgba(0, 0, 0, 0.25);
+  box-shadow: 7px 7px 20px 0px rgba(0, 0, 0, 0.25);
+  cursor: pointer;
+}
+p {
+  opacity: 0.8;
+  line-height: 1.6;
+  max-width: 30rem;
+}
+.landing {
+  height: 100vh;
+  width: 90vw;
+  margin-inline: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+}
+h1 {
+  color: white;
+  font-size: 5rem;
+  font-family: 'EquitanSans', 'League Spartan', sans-serif;
+  filter: drop-shadow(0 0 0.5rem rgba(0, 0, 0, 0.25));
+}
+.right {
+  text-align: right;
+}
+.bg {
+  width: 100vw;
+  height: calc(100vh + 10rem);
+  object-fit: cover;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+}
+.sand_bg {
+  width: 100vw;
+  height: calc(100vh + 10rem);
+  object-fit: cover;
+  position: absolute;
+  top: 100vh;
+  left: 0;
+  z-index: -2;
+}
 a {
   width: 100vw;
   height: 100vh;
